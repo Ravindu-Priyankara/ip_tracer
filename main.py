be81890a47a9bdb9d89aefd811159ffe0aecf870
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2022-12-14 19:58:04 +0530

    add basic lists

--- main.py
+++ main.py
@@ -1,33 +1,60 @@
 #! usr/bin/env python3
 
 try:
+    import pyfiglet
+    import urllib.request
+    from tabulate import tabulate
     from modules.user_loc import *
     from modules.color_codes import *
 except Exception as error:
-    print(error.__class__.__name__+":"+error.message)
+    #print(error.__class__.__name__+":"+error.message)
+    print(error)
 class Main:
     global cmd
     global banner
+    global map_func
     global user_data
+    global connection
 
     def main():
         #create a banner
 
         #find my details
-        user_data()
+        #print( user_data() if connection() else "no internet!" ) #simplyfy this code
+        #user_data()
+        
+        banner()
 
     def banner():
-        pass
+        ascii_banner = pyfiglet.figlet_format("IP TRACER")
+        print(BRIGHT_CYAN+ascii_banner+BRIGHT_GREEN+"by Ravindu Priyankara\n"+BRIGHT_WHITE)
+
+        table = [['Number', 'Details'], [1,"Command Line application"], [2,"Web Application"],[3,"Graphical user interface"]]
+        print(tabulate(table, headers='firstrow', tablefmt='fancy_grid'))
+
     def user_data():
         user_detail = get_location()
 
-        list = ["ip","city","region","region_code","country_code","country_code_iso3","country_name","country_capital","country_tld","continent_code","in_eu","postal","latitude","longitude","timezone","utc_offset","country_calling_code","currency","currency_name","languages","asn","org"]
+        list = ["ip","city","region","postal","timezone","organization","location"]
 
         for x in range(len(list)):
             print(f"{BRIGHT_YELLOW} your {list[x]} is: {BRIGHT_MAGENTA}{user_detail[list[x]]}\n")
+
+        #get a latitude and longitude
+
+        #loc_split = user_detail["location"]
+        #latitude,longitude = loc_split.split(",")
+
     
     def cmd():
         pass
+
+    def connection(host='http://google.com'):
+        try:
+            urllib.request.urlopen(host)
+            return True
+        except:
+            return False
     
 if __name__ == '__main__':
     mainClass = Main
--- modules/user_loc.py
+++ modules/user_loc.py
@@ -13,30 +13,16 @@ def get_ip_addr():
 #collect user data
 def get_location():
     ip_addr = get_ip_addr()
-    response = requests.get(f'https://ipapi.co/{ip_addr}/json/').json()
+    response = requests.get(f'https://ipinfo.io/{ip_addr}/json/').json()
     location_data = {
         "ip":ip_addr,
         "city":response.get('city'),
         "region":response.get('region'),
-        "region_code" : response.get('region_code'),
-        "country_code" : response.get('country_code'),
-        "country_code_iso3" : response.get('country_code_iso3'),
-        "country_name" : response.get('country_name'),
-        "country_capital" : response.get('country_capital'),
-        "country_tld" : response.get('country_tld'),
-        "continent_code" : response.get('continent_code'),
-        "in_eu" : response.get('in_eu'),
+        "country":response.get('country'),
         "postal" : response.get('postal'),
-        "latitude" : response.get('latitude'),
-        "longitude" : response.get('longitude'),
         "timezone" : response.get('timezone'),
-        "utc_offset" : response.get('utc_offset'),
-        "country_calling_code" : response.get('country_calling_code'),
-        "currency" : response.get('currency'),
-        "currency_name" : response.get('currency_name'),
-        "languages" : response.get('languages'),
-        "asn" : response.get('asn'),
-        "org" : response.get('org')
+        "organization" : response.get('org'),
+        "location": response.get('loc')
     }
     
     return location_data
